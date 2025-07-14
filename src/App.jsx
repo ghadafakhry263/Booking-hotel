aa1a553858ee9a39dc4b9a8ef01ecba1c12cf3b7
ghadafakhry263 <ghadadodo524@gmail.com>
2025-07-14 18:11:54 +0200

    handle sign in

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { NavLink } from "react-bootstrap"
+import Navbar from "./Component/Navbar"
+import { Route, Routes, useLocation } from "react-router-dom"
+import Home from "./Pages/Home"
 
 
 function App() {
-
+  // Check if the current path includes "owner"
+const isOwnerPath=useLocation().pathname.includes("owner")
 
   return (
     <>
-     <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    {!isOwnerPath &&< Navbar />}
+  <div className="min-h-[70vh]">
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </div>
     </>
   )
 }
